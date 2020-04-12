9077764d56753e9013cbcf10b173f3690a126959
Romain FLEURETTE <romainfleurette@univ-paris13.fr>
2020-04-12 20:03:44 +0200

    Icone Offline

--- vue/src/plugins/vuetify.js
+++ vue/src/plugins/vuetify.js
@@ -1,3 +1,4 @@
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import VueI18n from 'vue-i18n';
@@ -45,6 +46,9 @@ const i18n = new VueI18n({
 })
 
 export default new Vuetify({
+  icons:{
+    iconfont: 'mdi',
+  },
   lang: {
     t: (key, ...params) => i18n.t(key, params),
   },
